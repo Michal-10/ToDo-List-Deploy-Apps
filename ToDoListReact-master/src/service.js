2d105d7777843c7389ef282872999830649183a2
Michal-10 <m0548564934@gmail.com>
2025-02-09 00:36:53 +0200

    updates

--- ToDoListReact-master/src/service.js
+++ ToDoListReact-master/src/service.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-console.log(process.env.REACT_APP_API_URL);
-
 
 // הוספת Interceptor לתפיסת שגיאות ורישום ללוג
 axios.interceptors.response.use(
